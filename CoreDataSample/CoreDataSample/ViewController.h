4f8e2cdc8c258073c92969dcbc41bdf13af59003
Preejith Augustine <Preejith@ubuntu.citrus.com>
2016-05-11 15:01:32 -0700

    Added sample project

---
+++ CoreDataSample/CoreDataSample/ViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  CoreDataSample
+//
+//  Created by Preejith Augustine on 08/05/16.
+//  Copyright © 2016 Preejith Augustine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface ViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+
+
+
+
+
+
+
+
+
+
+@end
+
