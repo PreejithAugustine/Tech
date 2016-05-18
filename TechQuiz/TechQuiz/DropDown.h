d22b26d19d6244234a63c9029d5ff564327671cf
Preejith Augustine <Preejith@ubuntu.citrus.com>
2016-05-18 14:40:42 +0530

    To add dropdown for Categories

---
+++ TechQuiz/TechQuiz/DropDown.h
@@ -0,0 +1,13 @@
+//
+//  DropDown.h
+//  TechQuiz
+//
+//  Created by Preejith Augustine on 18/05/16.
+//  Copyright © 2016 Preejith Augustine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropDown : UIView
+
+@end
---
+++ TechQuiz/TechQuiz/DropDown.m
@@ -0,0 +1,13 @@
+//
+//  DropDown.m
+//  TechQuiz
+//
+//  Created by Preejith Augustine on 18/05/16.
+//  Copyright © 2016 Preejith Augustine. All rights reserved.
+//
+
+#import "DropDown.h"
+
+@implementation DropDown
+
+@end
